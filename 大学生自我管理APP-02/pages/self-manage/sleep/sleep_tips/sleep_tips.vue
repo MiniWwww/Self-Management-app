d26f675db137c87ad49e98f395e249900108de98
sigeludan <3321386552@qq.com>
2023-05-09 15:15:48 +0800

    feat:'完成自我管理模块睡眠部分‘睡眠小tips’'

--- 大学生自我管理APP-02/pages/self-manage/sleep/sleep_tips/sleep_tips.vue
+++ 大学生自我管理APP-02/pages/self-manage/sleep/sleep_tips/sleep_tips.vue
@@ -1,7 +1,7 @@
 <template>
 	<view>
 		<uni-list>
-			<uni-list-item show-arrow="true" >
+			<uni-list-item show-arrow="true" link to="/pages/self-manage/sleep/harm-of-staying-up-late/harm-of-staying-up-late">
 				<template v-slot:header>
 					<image class="img" src="/static/熬夜.jpg" mode="aspectFill"></image>
 				</template>
@@ -9,7 +9,7 @@
 					<text class="title">熬夜的危害</text>
 				</template>
 			</uni-list-item>
-			<uni-list-item show-arrow="true" >
+			<uni-list-item show-arrow="true" link to="/pages/self-manage/sleep/save/save">
 				<template v-slot:header>
 					<image class="img" src="/static/熬夜冠军.jpg" mode="aspectFill"></image>
 				</template>
@@ -17,7 +17,7 @@
 					<text class="title">熬夜补救指南</text>
 				</template>
 			</uni-list-item>
-			<uni-list-item show-arrow="true" >
+			<uni-list-item show-arrow="true" link to="/pages/self-manage/sleep/help/help">
 				<template v-slot:header>
 					<image class="img" src="/static/睡觉.jpg" mode="aspectFill"></image>
 				</template>
@@ -25,7 +25,14 @@
 					<text class="title">助眠小技巧</text>
 				</template>
 			</uni-list-item>
-			
+			<uni-list-item show-arrow="true" link to="/pages/self-manage/sleep/tips-for-insomnia/tips-for-insomnia">
+				<template v-slot:header>
+					<image class="img" src="/static/失眠.jpg" mode="aspectFill"></image>
+				</template>
+				<template v-slot:body>
+					<text class="title">失眠小妙招</text>
+				</template>
+			</uni-list-item>
 		</uni-list>
 		
 	</view>
@@ -61,8 +68,8 @@
 	.img{
 		flex-shrink: 0;
 		margin-right: 10px;
-		width: 100px;
-		height: 100px;
+		width: 80px;
+		height: 80px;
 		border-radius: 6px;
 		overflow: hidden;
 		border:1px #f5f5f5 solid;
---
+++ 大学生自我管理APP-02/pages/self-manage/sleep/tips-for-insomnia/tips-for-insomnia.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<uni-card title="失眠小妙招" sub-title="cr:网络" thumbnail="/static/失眠.jpg" is-shadow>
+			<image src="../../../../static/失眠.jpg" mode="widthFix" style="width: 280px;"></image>
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
