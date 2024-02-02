b2eaf6030cfc001243ac053479e480764d655b15
Fanrito <2902566341@qq.com>
2024-02-02 21:16:12 +0800

    feat: 新增清空课程表、修改时间、单双周等功能

--- 大学生自我管理APP-02/pages/index/dao/dao.js
+++ 大学生自我管理APP-02/pages/index/dao/dao.js
@@ -22,7 +22,7 @@ const dao = {
 	// 创建schedule表
 	createTableSchedule() {
 		var dbTable = 'schedule'
-		var data = 'id VARCHAR(255) PRIMARY KEY,'
+		var data = 'id INT PRIMARY KEY,'
 					+ 'begin INT NOT NULL,'
 					+ 'end INT NOT NULL,'
 					+ 'week INT NOT NULL,'
@@ -31,14 +31,43 @@ const dao = {
 					+ 'week_end INT NOT NULL,'
 					+ 'place VARCHAR(255) NOT NULL,'
 					+ 'teacher VARCHAR(255) NOT NULL,'
-					+ 'credit INT NOT NULL'
+					+ 'credit INT NOT NULL,'
+					+ 'odd_or_even INT NOT NULL'
 		const res = db.createTable(dbTable, data)
 		return res;
 	},
-	// todo 创建时间段表
+	// 创建时间段表
+	createTableTime() {
+		var dbTable = 'time'
+		var data = 'id INT PRIMARY KEY,'
+					+ 'begin VARCHAR(20) NOT NULL,'
+					+ 'end VARCHAR(20) NOT NULL'
+		const res = db.createTable(dbTable, data)
+		console.log(res);
+		
+		// 插入初始化数据
+		try{
+			db.insertTableData(dbTable, '1,' + '"08:00",' + '"08:45"')
+			db.insertTableData(dbTable, '2,' + '"08:55",' + '"09:40"')
+			db.insertTableData(dbTable, '3,' + '"10:10",' + '"10:55"')
+			db.insertTableData(dbTable, '4,' + '"11:05",' + '"11:50"')
+			db.insertTableData(dbTable, '5,' + '"14:30",' + '"15:15"')
+			db.insertTableData(dbTable, '6,' + '"15:25",' + '"16:10"')
+			db.insertTableData(dbTable, '7,' + '"16:40",' + '"17:25"')
+			db.insertTableData(dbTable, '8,' + '"17:35",' + '"18:20"')
+			db.insertTableData(dbTable, '9,' + '"19:10",' + '"19:55"')
+			db.insertTableData(dbTable, '10,' + '"20:05",' + '"20:50"')
+			db.insertTableData(dbTable, '11,' + '"21:20",' + '"22:05"')
+			db.insertTableData(dbTable, '12,' + '"22:15",' + '"23:00"')
+		}catch(e){
+			console.log("初始数据已插入");
+		}
+		
+		return res;
+	},
 	
 	// 插入课程
-	addCourse(begin, end, week, name, week_begin, week_end, place, teacher, credit) {
+	addCourse(begin, end, week, name, week_begin, week_end, place, teacher, credit, odd_or_even) {
 		const id = uuidv4();
 		var dbTable = 'schedule'
 		var data = '"' + id + '",'
@@ -50,14 +79,15 @@ const dao = {
 					+ week_end + ',"'
 					+ place + '","'
 					+ teacher + '",'
-					+ credit
+					+ credit + ','
+					+ odd_or_even
 		// console.log(data);
 		let res = db.insertTableData(dbTable, data)
 		return res
 	},
 	
 	// 修改课程
-	updateCourse(id, begin, end, week, name, week_begin, week_end, place, teacher, credit) {
+	updateCourseById(id, begin, end, week, name, week_begin, week_end, place, teacher, credit, odd_or_even) {
 		var dbTable = 'schedule'
 		var data = 'begin='
 					+ begin + ',end='
@@ -68,7 +98,21 @@ const dao = {
 					+ week_end + ',place="'
 					+ place + '",teacher="'
 					+ teacher + '",credit='
-					+ credit
+					+ credit + ',odd_or_even='
+					+ odd_or_even
+		console.log(data);
+		var lname = 'id'
+		var lvalue = id
+		let res = db.updateTableData(dbTable, data, lname, lvalue)
+		return res
+	},
+	
+	// 修改时间
+	updateCourse(id, begin, end) {
+		var dbTable = 'time'
+		var data = 'begin="'
+					+ begin + '",end="'
+					+ end +'"'
 		var lname = 'id'
 		var lvalue = id
 		let res = db.updateTableData(dbTable, data, lname, lvalue)
@@ -82,6 +126,20 @@ const dao = {
 		return res
 	},
 	
+	// 获取全部课程
+	findCourseById(id) {
+		var dbTable = 'schedule'
+		let res = db.selectTableData(dbTable, "id", id)
+		return res
+	},
+	
+	// 获取全部时间段
+	queryAllTime() {
+		var dbTable = 'time'
+		let res = db.selectTableData(dbTable)
+		return res
+	},
+	
 	// 删除课程
 	deleteCourseById(id) {
 		var dbTable = 'schedule'
@@ -89,6 +147,12 @@ const dao = {
 		var lvalue = id
 		let res = db.deleteTableData(dbTable, lname, lvalue)
 		return res
+	},
+	
+	// 清空课程表
+	truncateAllCourses() {
+		db.truncateTable('schedule');
+		this.createTableSchedule()
 	}
 	
 }
--- 大学生自我管理APP-02/pages/index/index.vue
+++ 大学生自我管理APP-02/pages/index/index.vue
@@ -1,10 +1,34 @@
 <template>
 	<view>
-		<SqTimetable @click="getCourse" :time="time" :image="image" :timeColor="timeColor" :courseList="courseList"
+		<SqTimetable @click="getCourse" @getTime="getTime" :time="time" :image="image" :timeColor="timeColor" :courseList="courseList"
 			:showNotWeek="true"></SqTimetable>
 		<!-- 字体图标 -->
 		<view class="create-todo" @click="creat"><text class="iconfont iconhao1"
 				:class="{ 'create-todo-active': textShow }">+</text></view>
+		<view class="truncate" @click="open"><uni-icons type="trash" color="white" size="30"></uni-icons></view>
+		<uni-popup ref="popupTime" type="center">
+			<uni-card style="display: flex; justify-content: center;">
+				<picker mode="time" value="{{beginTime}}" bindchange="bindBeginTimeChange">
+				    <uni-card>
+				      开始时间: {{time[timeIndex][0]}}
+				    </uni-card>
+				</picker>
+				<picker mode="time" value="{{endTime}}" bindchange="bindEndTimeChange">
+				    <uni-card>
+				      结束时间: {{time[timeIndex][1]}}
+				    </uni-card>
+				</picker>
+				<uni-button type="danger" @click="truncate">确定</uni-button>
+				<uni-button type="primary" @click="cancelTruncate">取消</uni-button>
+			</uni-card>
+		</uni-popup>
+		<uni-popup ref="popup" type="center">
+			<uni-card style="display: flex; justify-content: center;">
+				<text style="font-size: 20px;">确定要清空课程表吗？(此操作不可撤销)</text>
+				<uni-button type="danger" @click="truncate">确定</uni-button>
+				<uni-button type="primary" @click="cancelTruncate">取消</uni-button>
+			</uni-card>
+		</uni-popup>
 		<!-- 输入框 -->
 		<view class="create-content" style="z-index: 1" v-if="activeInput" :class="{ 'create-show': textShow }">
 			<view class="create-content-box">
@@ -21,20 +45,27 @@
 					<uni-forms-item required label="课程名" name="name">
 						<uni-easyinput type="text" v-model="formData.name" placeholder="请填写课程名" />
 					</uni-forms-item>
+					<uni-forms-item required label="单双周" name="odd_or_even">
+						<uni-data-select
+						      v-model="formData.odd_or_even"
+						      :localdata="range"
+						      @change="change"
+						    ></uni-data-select>
+					</uni-forms-item>
 					<uni-forms-item required label="起始周" name="week_begin">
-						<uni-easyinput type="number" v-model="formData.week_begin" placeholder="请填写课程名" />
+						<uni-easyinput type="number" v-model="formData.week_begin" placeholder="这节课从第几周开始？" />
 					</uni-forms-item>
 					<uni-forms-item required label="终止周" name="week_end">
-						<uni-easyinput type="number" v-model="formData.week_end" placeholder="请填写课程名" />
+						<uni-easyinput type="number" v-model="formData.week_end" placeholder="这节课到第几周结束？" />
 					</uni-forms-item>
 					<uni-forms-item required label="上课地点" name="place">
-						<uni-easyinput type="text" v-model="formData.place" placeholder="请填写课程名" />
+						<uni-easyinput type="text" v-model="formData.place" placeholder="请填写上课地点" />
 					</uni-forms-item>
 					<uni-forms-item required label="授课老师" name="teacher">
-						<uni-easyinput type="text" v-model="formData.teacher" placeholder="请填写课程名" />
+						<uni-easyinput type="text" v-model="formData.teacher" placeholder="请填写授课老师" />
 					</uni-forms-item>
 					<uni-forms-item required label="学分" name="credit">
-						<uni-easyinput type="number" v-model="formData.credit" placeholder="请填写课程名" />
+						<uni-easyinput type="number" v-model="formData.credit" placeholder="请填写学分" />
 					</uni-forms-item>
 				</uni-forms>
 				<view class="creat-button" v-if="creatButtonShow" @click="submit">创建</view>
@@ -57,6 +88,15 @@
 	
 	const form = ref(null)
 	
+	const popup = ref()
+	const popupTime = ref()
+	const open = () => {
+		popup.value.open()
+	}
+	const openPopupTiem = () => {
+		popupTime.value.open()
+	}
+	
 	const formData = ref({
 		begin: null,
 		end: null,
@@ -66,8 +106,28 @@
 		week_end: null,
 		place: '',
 		teacher: '',
-		credit: null
+		credit: null,
+		odd_or_even: 0
 	})
+	
+	const timeIndex = ref(0)
+	const beginTime = ref("")
+	const endTime = ref("")
+	const bindBeginTimeChange = (e) => {
+		console.log('开始时间改变' + e.detail.value);
+		beginTime.value = e.detail.value
+		console.log(beginTime);
+	}
+	const bindEndTimeChange = (e) => {
+		console.log('结束时间改变' + e.detail.value);
+		endTime.value = e.detail.value
+	}
+	
+	const range = ref([
+	        { value: 0, text: "每周" },
+	        { value: 1, text: "单周" },
+	        { value: 2, text: "双周" },
+	])
 
 	const rules = {
 		// 对begin字段进行必填验证
@@ -181,6 +241,20 @@
 		loadCourses()
 		close()
 	}
+	
+	const truncate = () => {
+		dao.truncateAllCourses()
+		cancelTruncate()
+		loadCourses()
+	}
+	
+	const cancelTruncate = () => {
+		popup.value.close()
+	}
+	
+	const change = (e) => {
+		formData.value.odd_or_even = e
+	}
 
 	const activeInput = ref(false)
 	const textShow = ref(false)
@@ -206,21 +280,14 @@
 		creatButtonShow.value = false
 		updateButtonShow.value = true
 	};
+	
+	const getTime = (item) => {
+		timeIndex.value = item - 1
+		openPopupTiem()
+		console.log(item);
+	}
 
-	const time = [
-		['08:00', '08:45'],
-		['08:55', '09:40'],
-		['10:10', '10:55'],
-		['11:05', '11:50'],
-		['14:30', '15:15'],
-		['15:25', '16:10'],
-		['16:40', '17:25'],
-		['17:35', '18:20'],
-		['19:10', '19:55'],
-		['20:05', '20:50'],
-		['21:20', '22:05'],
-		['22:15', '23:00']
-	];
+	const time = ref([])
 
 	const image =
 		'https://img04.sogoucdn.com/app/a/100520093/ca86e620b9e623ff-f4709b64c5c0ee80-852ef8b2f248787b2f289ae80f88038c.jpg';
@@ -262,13 +329,29 @@
 			console.log("数据库app已启动");
 		}
 		dao.createTableSchedule()
+		dao.createTableTime()
 		loadCourses()
+		loadTime()
 	})
 	
 	const loadCourses = () => {
 		let resPromise = dao.queryAllCourses()
 		resPromise.then(res => {
 			courseList.value = res
+		}).catch(err => {
+			console.error(err);
+		})
+	}
+	
+	const loadTime = () => {
+		let resPromise = dao.queryAllTime()
+		resPromise.then(res => {
+			// time.value = res
+			console.log(res);
+			time.value = [];
+			res.forEach(item => {
+				time.value.push([item.begin, item.end])
+			})
 		}).catch(err => {
 			console.error(err);
 		})
@@ -284,7 +367,8 @@
 		let place = course.place
 		let teacher = course.teacher
 		let credit = course.credit
-		let resPromise = dao.addCourse(begin, end, week, name, week_begin, week_end, place, teacher, credit)
+		let odd_or_even = course.odd_or_even
+		let resPromise = dao.addCourse(begin, end, week, name, week_begin, week_end, place, teacher, credit, odd_or_even)
 	}
 
 	const updateCourse = (course) => {
@@ -298,7 +382,8 @@
 		let place = course.place
 		let teacher = course.teacher
 		let credit = course.credit
-		let resPromise = dao.updateCourse(id, begin, end, week, name, week_begin, week_end, place, teacher, credit)
+		let odd_or_even = course.odd_or_even
+		let resPromise = dao.updateCourseById(id, begin, end, week, name, week_begin, week_end, place, teacher, credit, odd_or_even)
 	}
 
 	const deleteCourse = (course) => {
@@ -308,6 +393,31 @@
 </script>
 
 <style>
+	.truncate {
+		position: fixed;
+		bottom: 80rpx;
+		right: 40rpx;
+		width: 90rpx;
+		height: 90rpx;
+		line-height: 90rpx;
+		text-align: center;
+		border-radius: 50%;
+		color: #fff;
+		font-size: 30rpx;
+		z-index: 999997;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		bottom: 80px;
+		right: 20;
+		margin: 0 auto;
+		width: 50px;
+		height: 50px;
+		border-radius: 50%;
+		background-color: #009688;
+		box-shadow: 0px 5px 10px rgba(0, 150, 136, 0.4);
+	}
+
 	.create-todo {
 		position: fixed;
 		bottom: 80rpx;
@@ -317,20 +427,13 @@
 		line-height: 90rpx;
 		text-align: center;
 		border-radius: 50%;
-		background-color: #009688;
 		color: #fff;
-		font-size: 70rpx;
+		font-size: 50rpx;
 		z-index: 999997;
-		box-shadow: 0px 5px 10px rgba(0, 150, 136, 0.4);
-	}
-
-	.create-todo {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		position: fixed;
 		bottom: 20px;
-
 		right: 20;
 		margin: 0 auto;
 		width: 50px;
